bbf1ba8d5effafc21c6a09e808ec585309dcf589
Nazmul-dot <nazmulhaq606845@gmail.com>
2025-05-12 23:22:33 +0600

    add jenkins project

---
+++ src/main/java/com/example/jenkins_test/user/UserController/Controller.java
@@ -0,0 +1,17 @@
+package com.example.jenkins_test.user.UserController;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class Controller {
+
+    @GetMapping("/jenkins")
+    public ResponseEntity<?> getUser() {
+        return new ResponseEntity<>("Jenkins", HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/jenkins_test/user/tt.java
@@ -0,0 +1,4 @@
+package com.example.jenkins_test.user;
+
+public class tt {
+}
